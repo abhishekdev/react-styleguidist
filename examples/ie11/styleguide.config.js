aebf21c380c694a1ba52a047cdb951cb131802ed
Abhishek Dev <227926+abhishekdev@users.noreply.github.com>
2019-04-16 23:18:43 -0400

    Update IE11 sample to work with a variety of webpack 4.x configs
    - Prepend a specific loader to support IE11 to `webpack.config.module.rules`
    - Set the babel config option in the loader rules to avoid conflicting changes with complex babel-config files. This also works with `.babelrc.js`
    - This handles cases for webpack nested rules. Ref: https://webpack.js.org/configuration/module/#nested-rules
    
    [Ref #1327]

--- examples/ie11/styleguide.config.js
+++ examples/ie11/styleguide.config.js
@@ -1,7 +1,29 @@
 const webpackConfig = require('./webpack.config');
 
-const babel = webpackConfig.module.rules.filter(rule => rule.loader === 'babel-loader')[0];
-babel.exclude = /node_modules\/(?!(ansi-styles|strip-ansi|ansi-regex|react-dev-utils|chalk|regexpu-core|unicode-match-property-ecmascript|unicode-match-property-value-ecmascript|acorn-jsx)\/).*/;
+/**
+ * NOTE: To support IE11, transform all react-styleguidist modules and dependencies.
+ *   without this config, IE11 throws Syntax errors from the following libraries:
+ * - acorn - class syntax
+ * - regexpu-core - arrow function
+ * - unicode-match-property-ecmascript - string interpolation
+ * - unicode-match-property-value-ecmascript - string interpolation
+ * - react-dev-utils - module syntax and arrow function in `strip-ansi`, `ansi-regex`
+ * - ansi-styles - arrow function
+ * - ansi-regex - arrow function
+ * - chalk - const usage in for...of (Runtime error : const must be initialized)
+ * - strip-ansi - arrow function
+ */
+const TRANSFORMS_FOR_IE11 = {
+	test: /\.jsx?$/,
+	include: /node_modules\/(?=(acorn-jsx|regexpu-core|unicode-match-property-ecmascript|unicode-match-property-value-ecmascript|react-dev-utils|ansi-styles|ansi-regex|chalk|strip-ansi)\/).*/,
+	use: {
+		loader: 'babel-loader',
+		options: {
+			presets: ['@babel/preset-env'],
+		},
+	},
+};
+webpackConfig.module.rules = [TRANSFORMS_FOR_IE11, ...webpackConfig.module.rules];
 
 module.exports = {
 	title: 'React Style Guide Example',
